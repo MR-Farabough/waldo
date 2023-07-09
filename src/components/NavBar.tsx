0d4668620ff856c49b18a24fcd852a4838ca7b58
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-09 14:45:29 -0500

    NavBar logic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,27 @@
-const NavBar = () => {
-	return <div>NavBar</div>;
+import '../styles/index.css';
+import { useState } from 'react';
+
+interface Props {
+	title: string;
+	gameSelection: string;
+	timer: boolean;
+}
+
+const NavBar = ({ title, gameSelection, timer }: Props) => {
+	let [time, setTime] = useState(0);
+	const startTimer = () => {
+		setTimeout(() => {
+			setTime(time + 1);
+		}, 1000);
+		return time;
+	};
+	return (
+		<div className="navBar">
+			<h1 className="navText">{gameSelection}</h1>
+			<h1 className="navText">{title}</h1>
+			{timer ? <h1 className="navText">{startTimer()}</h1> : <h1></h1>}
+		</div>
+	);
 };
 
 export default NavBar;
