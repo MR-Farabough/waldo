8e7ba7608a518c7c91e4858dda1d6699e2432f68
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-09 23:16:02 -0500

    Basic Card layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,17 @@
-const Card = () => {
-	return <div>Card</div>;
+import '../styles/Card.css';
+
+interface Props {
+	title: string;
+	picture: string;
+}
+
+const Card = ({ title, picture }: Props) => {
+	return (
+		<div className="card">
+			<img className="preview" src={picture} alt="Game-Picture" />
+			<h1 className="card-title">{title}</h1>
+		</div>
+	);
 };
 
 export default Card;
