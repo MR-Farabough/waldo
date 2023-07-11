49da3b42868a8a820cdaeb1ee28dfd0d44dac35a
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-10 20:11:09 -0500

    Get cords of click

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,33 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import '../styles/Game.css';
-import NavBar from './NavBar';
 
 const Game = () => {
 	const { state } = useLocation();
-	console.log(state);
-	<NavBar title="Where's Waldo" gameSelection={state.title} timer={true} />;
-
+	const roundToThousandth = (num: number) => {
+		return Math.round((num + Number.EPSILON) * 1000) / 1000;
+	};
+	useEffect(() => {
+		const getClickPosition = (e: any) => {
+			const gamePicture = document.querySelector('.game-left');
+			if (gamePicture) {
+				const rect = gamePicture.getBoundingClientRect();
+				const xPosition = (e.clientX - rect.left) / rect.width;
+				const yPosition = (e.clientY - rect.top) / rect.height;
+				console.log(roundToThousandth(xPosition), roundToThousandth(yPosition));
+			}
+		};
+		const gamePicture = document.querySelector('.game-left');
+		if (gamePicture) {
+			gamePicture.addEventListener('click', getClickPosition);
+		}
+		// Cleanup function to remove the event listener when the component unmounts
+		return () => {
+			if (gamePicture) {
+				gamePicture.removeEventListener('click', getClickPosition);
+			}
+		};
+	}, []);
 	return (
 		<main className="main-game">
 			<section className="game-left">
