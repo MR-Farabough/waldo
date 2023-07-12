edb4180060bf2a115aba4be630bbfea6ee7de0e0
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-12 16:01:53 -0500

    Changed Title

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -1,7 +1,7 @@
 import Card from './Card';
 
 const GameCards = () => {
-	const gamePictures = [['src/assets/main.jpg', 'The Track']];
+	const gamePictures = [['src/assets/main.jpg', 'The Pond']];
 	const builtCards = gamePictures.map((game, index) => {
 		return <Card title={game[1]} picture={game[0]} key={index} />;
 	});
