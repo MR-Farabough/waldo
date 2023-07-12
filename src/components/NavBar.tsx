1d00f1a763da39b149df865c0ac5f1fdc7f8e731
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-11 22:52:29 -0500

    Made title a link to home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import '../styles/index.css';
 import { useState } from 'react';
 
@@ -8,6 +9,10 @@ interface Props {
 }
 
 const NavBar = ({ title, gameSelection, timer }: Props) => {
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate('/');
+	};
 	let [time, setTime] = useState(0);
 	const startTimer = () => {
 		setTimeout(() => {
@@ -18,7 +23,9 @@ const NavBar = ({ title, gameSelection, timer }: Props) => {
 	return (
 		<nav className="navBar">
 			<h1 className="navText">{gameSelection}</h1>
-			<h1 className="navText">{title}</h1>
+			<h1 onClick={handleClick} className="navText home">
+				{title}
+			</h1>
 			{timer ? <h1 className="navText">{startTimer()}</h1> : <h1></h1>}
 		</nav>
 	);
