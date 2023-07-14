5b8114b55fc65883a9fe09ed04180b698155301f
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-13 21:21:03 -0500

    Added logic and hooked up backend

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,19 @@
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import waldo from '../assets/waldo.jpg';
 import wilma from '../assets/wilma.jpg';
 import wizard from '../assets/wizard.jpg';
 import '../styles/Game.css';
+import supabase from '../config/supabaseClient';
 
 const Game = () => {
+	const navigate = useNavigate();
+	const [, setUserSelection] = useState<number[]>([]);
+	const [value, setValue] = useState<any>(null);
 	const { state } = useLocation();
 	const characterList = ['Waldo', 'Wilma', 'Wizard'];
+	const gamePicture = document.querySelector('.game-left');
+
 	const roundToThousandth = (num: number) => {
 		return Math.round((num + Number.EPSILON) * 1000) / 1000;
 	};
@@ -21,7 +27,124 @@ const Game = () => {
 		document.body.appendChild(square);
 	}
 
-	function createCharacterDropDown(event: MouseEvent) {
+	const activateModal = (instruction: string) => {
+		const div = document.createElement('div');
+		div.classList.add('instruction-modal');
+		div.style.backgroundColor = 'green';
+		setTimeout(() => {
+			document.querySelector('.instruction-modal')?.remove();
+		}, 1000);
+		if (instruction === 'win') {
+			div.innerText = 'YOU WIN';
+			const time = document.querySelector('.timer')?.innerHTML;
+			setTimeout(() => {
+				navigate('/', { state: time });
+			}, 2000);
+		} else if (instruction == 'correct') {
+			div.innerText = 'Correct';
+		} else if (instruction == 'miss') {
+			div.style.backgroundColor = 'red';
+			div.innerText = 'Incorrect';
+		}
+		document.querySelector('.game-right')?.appendChild(div);
+	};
+
+	const checkUserSelection = (
+		characterChoice: string,
+		choosenCords: number[],
+		data: any[]
+	) => {
+		let comparisonData = undefined;
+		if (characterChoice == 'Wilma') {
+			comparisonData = data[0].cord;
+		} else if (characterChoice == 'Waldo') {
+			comparisonData = data[1].cord;
+		} else {
+			comparisonData = data[2].cord;
+		}
+		const newArr = comparisonData.split(' ');
+		const checkOne = newArr[0] < choosenCords[0] && choosenCords[0] < newArr[1];
+		const checkTwo = newArr[2] < choosenCords[1] && choosenCords[1] < newArr[3];
+		const characterTexts = document.querySelectorAll('.character-text');
+		if (checkOne && checkTwo) {
+			characterTexts.forEach((nameEl) => {
+				if (nameEl.innerHTML == characterChoice) {
+					activateModal('correct');
+					nameEl.innerHTML += '✅';
+					if (
+						characterTexts[0].innerHTML[
+							characterTexts[0].innerHTML.length - 1
+						] == '✅' &&
+						characterTexts[1].innerHTML[
+							characterTexts[1].innerHTML.length - 1
+						] == '✅' &&
+						characterTexts[2].innerHTML[
+							characterTexts[2].innerHTML.length - 1
+						] == '✅'
+					) {
+						activateModal('win');
+					}
+				}
+			});
+		}
+		if (!checkOne || !checkTwo) {
+			activateModal('miss');
+		}
+
+		// Check if every character name has a check mark
+	};
+
+	function removeSelection() {
+		let div = document.querySelector('.selector');
+		let div2 = document.querySelector('.character-dropdown');
+		if (div && div2) {
+			div.remove();
+			div2.remove();
+		}
+	}
+
+	useEffect(() => {
+		const getData = async () => {
+			const { data, error } = await supabase.from('locations').select();
+			if (error) {
+				console.log(error);
+				setValue(null);
+			}
+			if (data) {
+				console.log('success');
+				setValue(data);
+			}
+		};
+		getData();
+
+		const getClickPosition = (e: any) => {
+			if (gamePicture) {
+				removeSelection();
+				removeSelection();
+				const rect = gamePicture.getBoundingClientRect();
+				const xPosition = (e.clientX - rect.left) / rect.width;
+				const yPosition = (e.clientY - rect.top) / rect.height;
+				const userCords = [
+					roundToThousandth(xPosition),
+					roundToThousandth(yPosition),
+				];
+				setUserSelection(() => {
+					const updatedSelection = [...userCords];
+					create(e);
+					createCharacterDropDown(e, updatedSelection);
+					return updatedSelection;
+				});
+			}
+		};
+
+		if (gamePicture) gamePicture.addEventListener('click', getClickPosition);
+	}, [gamePicture]);
+
+	const createCharacterDropDown = (
+		event: MouseEvent,
+		userSelection: number[]
+	) => {
+		// Create blank div for character list
 		let dropDown = document.createElement('div');
 		dropDown.classList.add('character-dropdown');
 		dropDown.style.position = 'absolute';
@@ -35,48 +158,17 @@ const Game = () => {
 			characterBtn.innerText = character;
 			dropDown.appendChild(characterBtn);
 		});
+		// Event Listener for dropdown buttons
 		const characterBtns = document.querySelectorAll('.character');
 		characterBtns.forEach((btn) => {
 			btn.addEventListener('click', () => {
 				removeSelection();
-				//TODO Handle Logic for character selection
+				removeSelection();
+				checkUserSelection(btn.innerHTML, userSelection, value);
 			});
 		});
-	}
+	};
 
-	function removeSelection() {
-		let div = document.querySelector('.selector');
-		let div2 = document.querySelector('.character-dropdown');
-		if (div && div2) {
-			div.remove();
-			div2.remove();
-		}
-	}
-
-	useEffect(() => {
-		const getClickPosition = (e: any) => {
-			const gamePicture = document.querySelector('.game-left');
-			if (gamePicture) {
-				removeSelection();
-				const rect = gamePicture.getBoundingClientRect();
-				const xPosition = (e.clientX - rect.left) / rect.width;
-				const yPosition = (e.clientY - rect.top) / rect.height;
-				console.log(roundToThousandth(xPosition), roundToThousandth(yPosition));
-				create(e);
-				createCharacterDropDown(e);
-			}
-		};
-		const gamePicture = document.querySelector('.game-left');
-		if (gamePicture) {
-			gamePicture.addEventListener('click', getClickPosition);
-		}
-		// Cleanup function to remove the event listener when the component unmounts
-		return () => {
-			if (gamePicture) {
-				gamePicture.removeEventListener('click', getClickPosition);
-			}
-		};
-	}, []);
 	return (
 		<main className="main-game">
 			<section className="game-left">
@@ -85,15 +177,15 @@ const Game = () => {
 			<section className="game-right">
 				<div className="character-div">
 					<img className="character-img" src={waldo} alt="waldo" />
-					<p>Waldo</p>
+					<p className="character-text">Waldo</p>
 				</div>
 				<div className="character-div">
 					<img className="character-img" src={wilma} alt="wilma" />
-					<p>Wilma</p>
+					<p className="character-text">Wilma</p>
 				</div>
 				<div className="character-div">
 					<img className="character-img" src={wizard} alt="wizard" />
-					<p>Wizard</p>
+					<p className="character-text">Wizard</p>
 				</div>
 			</section>
 		</main>
