8c60fd4e00081dfcb73271f51913f433d8d87d0e
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-13 21:19:30 -0500

    Added an alert for time spent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './styles/App.css';
+import { useLocation } from 'react-router-dom';
 import GameCards from './components/GameCards';
 
 function App() {
+	const { state } = useLocation();
 	return (
 		<>
+			{state && alert(`You solved it in ${state} seconds`)}
 			<GameCards />
 		</>
 	);
