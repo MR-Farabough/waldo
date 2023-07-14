430c16b6f9fc30d888e2b367be22e076e6517894
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-13 21:29:23 -0500

    Push to gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Router />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
